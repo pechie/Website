56c4930ee003c6ffb5946ecf6139f3319e62455c
Nikolas Pechie <pechie.n@northeastern.edu>
2020-12-27 01:13:23 -0500

    tutorial complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            Nikolas Pechie <i className="fab fa-typo3"></i>
+            Pechie <i className="fab fa-typo3"></i>
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
