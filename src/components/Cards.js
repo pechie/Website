126856ac6c6cf472e693600f7b1137dd393a58d6
Nikolas Pechie <pechie.n@northeastern.edu>
2020-12-30 17:16:06 -0500

    Added custom scrollbar

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,45 +1,78 @@
 import React from "react";
 import "./Cards.css";
+import { Scrollbars } from "react-custom-scrollbars";
 
 export default function Cards() {
   return (
     <div className="cards-container">
-      <CardItem
-        src="/images/nodejs-logo.png"
-        title="NodeJS"
-        text="9 months of experience with NodeJS at Integrity Security Services, 
+      <Scrollbars
+        className="scrollbar"
+        hideTracksWhenNotNeeded="true"
+        renderTrackVertical={({ style, ...props }) => (
+          <div
+            {...props}
+            style={{
+              ...style,
+              backgroundColor: "white",
+              right: "0px",
+              bottom: "2px",
+              top: "2px",
+              borderRadius: "3px",
+              width: "8px",
+            }}
+          />
+        )}
+        renderThumbVertical={({ style, ...props }) => (
+          <div
+            {...props}
+            style={{
+              ...style,
+              width: "4px",
+              borderRadius: "4px",
+              top: "2px",
+              left: "2px",
+              backgroundColor: "#082d3b",
+            }}
+          />
+        )}
+      >
+        <CardItem
+          src="/images/nodejs-logo.png"
+          title="NodeJS"
+          text="9 months of experience with NodeJS at Integrity Security Services, 
               and another six months on my senior project"
-      />
-      <CardItem
-        src="/images/react-logo.png"
-        title="React"
-        text="Self taught in React through following tutorials on YouTuber, 
+        />
+        <CardItem
+          src="/images/react-logo.png"
+          title="React"
+          text="Self taught in React through following tutorials on YouTuber, 
               culminating in creating this website"
-      />
-      <CardItem
-        src="/images/react-logo.png"
-        title="React"
-        text="Self taught in React through following tutorials on YouTuber, 
+        />
+        <CardItem
+          src="/images/react-logo.png"
+          title="React"
+          text="Self taught in React through following tutorials on YouTuber, 
               culminating in creating this website"
-      />
-      <CardItem
-        src="/images/react-logo.png"
-        title="React"
-        text="Self taught in React through following tutorials on YouTuber, 
+        />
+        <CardItem
+          src="/images/react-logo.png"
+          title="React"
+          text="Self taught in React through following tutorials on YouTuber, 
               culminating in creating this website"
-      />
-      <CardItem
-        src="/images/nodejs-logo.png"
-        title="NodeJS"
-        text="9 months of experience with NodeJS at Integrity Security Services, 
+        />
+        <CardItem
+          src="/images/nodejs-logo.png"
+          title="NodeJS"
+          text="9 months of experience with NodeJS at Integrity Security Services, 
               and another six months on my senior project"
-      />
-      <CardItem
-        src="/images/nodejs-logo.png"
-        title="NodeJS"
-        text="9 months of experience with NodeJS at Integrity Security Services, 
+        />
+        <CardItem
+          src="/images/nodejs-logo.png"
+          title="NodeJS"
+          text="9 months of experience with NodeJS at Integrity Security Services, 
               and another six months on my senior project"
-      />
+        />
+      </Scrollbars>
     </div>
   );
 }
