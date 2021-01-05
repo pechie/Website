521fa673ec4dd3dcc9248b9ffb124933facfa9cc
Nikolas Pechie <pechie.n@northeastern.edu>
2021-01-04 23:20:28 -0500

    Completed contact page

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -13,7 +13,8 @@ export default function ContactMe() {
           <h1>Email</h1>
           <p>
             Feel free to reach out with any questions you may have, you can
-            either use to link below, or send me an email at pechie717@gmail.com
+            either use to link below, or send me an email at
+            pechie717@gmail.com, and I will respond as soon as I can.
           </p>
           <a href="mailto:pechie717@gmail.com" target="_blank" rel="noreferrer">
             <button className="email-button" type="button">
@@ -22,7 +23,16 @@ export default function ContactMe() {
           </a>
         </div>
         <div className="contact-card-item">
-          <h1>Call me beep me</h1>
+          <h1>Phone</h1>
+          <p>
+            My cell phone number is below, send me a text at any hour of the
+            day, or give me a call/leave a message if I am not able to talk at
+            the moment.
+          </p>
+          <button className="phone-button" type="button">
+            <i class="fas fa-phone-square-alt"></i>
+            <a href="tel:+1-774-452-2647">(774)-452-2647</a>
+          </button>
         </div>
       </div>
     </div>
