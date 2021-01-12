a999da0b3922969c05d5f8288b3c93090a827a32
Nikolas Pechie <pechie.n@northeastern.edu>
2021-01-11 20:36:58 -0500

    Formatting work page on mobile

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
+// import "./App.css";
 import Home from "./components/pages/Home";
 import Resume from "./components/pages/Resume";
 import Experience from "./components/pages/Experience";
